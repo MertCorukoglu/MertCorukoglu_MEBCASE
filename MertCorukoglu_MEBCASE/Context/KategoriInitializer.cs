a2bda1bc8cb1ddd2032fb5fd54805c56fc6950f0
MertCorukoglu <me.corukoglu@gmailcom>
2022-03-26 01:38:16 +0300

    Add project files.

---
+++ MertCorukoglu_MEBCASE/Context/KategoriInitializer.cs
@@ -0,0 +1,25 @@
+﻿using MertCorukoglu_MEBCASE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MertCorukoglu_MEBCASE.Context
+{
+    public class KategoriInitializer : CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            List<Kategori> cats = new List<Kategori>() { new Kategori { KategoriAdi = "Kategori 1" }, new Kategori { KategoriAdi = "Kategori 2"} };
+
+            foreach (var cat in cats)
+            {
+                context.Kategoriler.Add(cat);
+            }
+            context.SaveChanges();
+            
+        }
+    }
+}
